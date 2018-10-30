05c684ad607dcf46660e7165a9ee583e2237bead
SkelSec <info@skelsec.com>
2018-10-30 23:12:05 +0000

    fixing stuff

--- responder3_log_discord/__init__.py
+++ responder3_log_discord/__init__.py
@@ -1,6 +1,8 @@
 # Work with Python 3.6
 import asyncio
 import discord
+import traceback
+
 from responder3.core.logtask import LoggerExtensionTask
 from responder3.core.commons import Credential, ConnectionOpened
 
@@ -57,27 +59,27 @@ class HoneyBot(discord.Client):
 		
 class discordHandler(LoggerExtensionTask):
 	def init(self):
-		self.msg_queue = asyncio.Queue()
-		self.token = self.config['token']
-		self.channel_name = self.config['channel']
-		self.extra_info = None
-		self.log_connections = False
-		if 'extra_info' in self.config:
-			self.extra_info = self.config['extra_info']
-		if 'log_connections' in self.config and self.config['log_connections'] == True:
-			self.log_connections = True
-		self.discordbot = HoneyBot(self.msg_queue, self.token, self.channel_name)
-		
+		try:
+			self.msg_queue = asyncio.Queue()
+			self.token = self.config['token']
+			self.channel_name = self.config['channel']
+			self.extra_info = None
+			self.log_connections = False
+			if 'extra_info' in self.config:
+				self.extra_info = self.config['extra_info']
+			if 'log_connections' in self.config and self.config['log_connections'] == True:
+				self.log_connections = True
+			self.discordbot = HoneyBot(self.msg_queue, self.token, self.channel_name)
+		except Exception as e:
+			traceback.print_exc()
+
 	async def main(self):
 		asyncio.ensure_future(self.discordbot.hb_start())
 		while True:
 			msg = await self.result_queue.get()
 			try:
 				if isinstance(msg, Credential):
-					print('HA! crez!')
-					print('[discordhandler] %s' % msg)
 					#https://anidiots.guide/first-bot/using-embeds-in-messages
-					
 					embed = discord.Embed(title = "Credntials", description="Here comes the next contestant!", color=3447003)
 					embed.set_author(name = "HoneyBot")
 					embed.add_field(name="Source IP", value=str(msg.client_addr), inline=False)
@@ -87,20 +89,20 @@ class discordHandler(LoggerExtensionTask):
 					embed.add_field(name="Extra info", value=str(self.extra_info), inline=False)
 					embed.set_footer(text= "© @SkelSec") #icon_url: client.user.avatarURL,
 					await self.msg_queue.put(embed)
-				
+
 				if isinstance(msg, ConnectionOpened) and self.log_connections == True:
 					embed = discord.Embed(title = "Connection", description="We have a visitor", color=3447003)
 					embed.set_author(name = "HoneyBot")
 					embed.add_field(name="Source IP", value=str(msg.connection.remote_ip), inline=False)
 					embed.add_field(name="Dst port", value= str(msg.connection.local_port), inline=False)
-					embed.add_field(name="Reverse DNS", value=str(msg.remote_dns), inline=False)
+					embed.add_field(name="Reverse DNS", value=str(msg.connection.remote_dns), inline=False)
 					embed.add_field(name="Extra info", value=str(self.extra_info), inline=False)
 					embed.set_footer(text= "© @SkelSec") #icon_url: client.user.avatarURL,
 					await self.msg_queue.put(embed)
-					
+
 			except Exception as e:
 				traceback.print_exc()
-	
+
 	async def setup(self):
 		pass
 
